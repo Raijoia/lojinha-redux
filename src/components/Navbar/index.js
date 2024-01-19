9ca03dd7b4b35533e401b8f99a07a9658bbd4470
Raí Joia <megac154@gmail.com>
2024-01-19 19:59:05 -0300

    loja(component): icons in navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,39 @@
+import classNames from 'classnames'
 import { ReactComponent as Logo } from '../../assets/logo.svg'
 import styles from './Navbar.module.scss'
+import {
+  RiShoppingCart2Line,
+  RiShoppingCartFill
+} from 'react-icons/ri'
+
+const iconeProps = {
+  color: 'white',
+  size: 24
+}
 
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
       <Logo className={styles.logo} />
+      <div className={styles.link}>
+        <a 
+          href="/" 
+          className={classNames(styles.link, {
+            [styles.selected]: window.location.pathname === '/'
+          })}
+        >Pagina Inicial</a>
+      </div>
+      <div className={styles.busca}>
+
+      </div>
+      <div className={styles.icones}>
+        <a href="/carrinho">
+          {window.location.pathname === '/carrinho' 
+            ? <RiShoppingCartFill {...iconeProps} /> 
+            : <RiShoppingCart2Line {...iconeProps} />
+          }
+        </a>
+      </div>
     </nav>
   )
 }
