004d02377a7c9488d315bd3c905072acf4f298f1
Raí Joia <megac154@gmail.com>
2024-01-23 09:58:52 -0300

    loha(link): add link as router-dom

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,6 +6,7 @@ import {
   RiShoppingCartFill
 } from 'react-icons/ri'
 import Busca from 'components/Busca'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
 const iconeProps = {
   color: 'white',
@@ -13,27 +14,30 @@ const iconeProps = {
 }
 
 export default function Navbar() {
+  const location = useLocation()
+  const navigate = useNavigate()
+
   return (
     <nav className={styles.nav}>
-      <Logo className={styles.logo} />
+      <Logo className={styles.logo} onClick={() => navigate('/')} />
       <div className={styles.link}>
-        <a 
-          href="/" 
+        <Link 
+          to="/" 
           className={classNames(styles.link, {
-            [styles.selected]: window.location.pathname === '/'
+            [styles.selected]: location.pathname === '/'
           })}
-        >Pagina Inicial</a>
+        >Pagina Inicial</Link>
       </div>
       <div className={styles.busca}>
           <Busca />
       </div>
       <div className={styles.icones}>
-        <a href="/carrinho">
+        <Link to="/carrinho">
           {window.location.pathname === '/carrinho' 
             ? <RiShoppingCartFill {...iconeProps} /> 
             : <RiShoppingCart2Line {...iconeProps} />
           }
-        </a>
+        </Link>
       </div>
     </nav>
   )
