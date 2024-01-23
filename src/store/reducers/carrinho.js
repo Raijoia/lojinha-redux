a53a98bcab85e93933f5ca28c2e1499b22dd4ce6
Raí Joia <megac154@gmail.com>
2024-01-23 14:05:29 -0300

    loja(reducer): finalizar compra add

--- src/store/reducers/carrinho.js
+++ src/store/reducers/carrinho.js
@@ -24,10 +24,11 @@ const carrinhoSlice = createSlice({
         if(itemNoCarrinho.id === payload.id) itemNoCarrinho.quantidade += payload.quantidade;
         return itemNoCarrinho;
       })
-    }
+    },
+    resetarCarrinho: () => estadoInicial
   }
 })
 
-export const { mudarCarrinho, mudarQuantidade } = carrinhoSlice.actions
+export const { mudarCarrinho, mudarQuantidade, resetarCarrinho } = carrinhoSlice.actions
 
 export default carrinhoSlice.reducer
